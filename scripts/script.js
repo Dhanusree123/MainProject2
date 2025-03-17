b2fdca51b1bc693f2335c0565ff829d0a7e69358
Dhanusree <dhanusree@microfox.co>
2025-03-17 10:28:15 +0530

    completed the task

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menuToggle = document.getElementById("menuToggle");
-    const menuOverlay = document.getElementById("menuOverlay");
-    const closeMenu = document.getElementById("closeMenu");
-    const menuItems = document.querySelectorAll(".menu-items li");
-
-    menuToggle.addEventListener("click", () => {
-        menuOverlay.classList.add("active");
-        menuItems.forEach((item, index) => {
-            setTimeout(() => {
-                item.style.opacity = "1";
-                item.style.transform = "translateX(0)";
-            }, 100 * index);
-        });
-    });
-
-    closeMenu.addEventListener("click", () => {
-        menuOverlay.classList.remove("active");
-        menuItems.forEach((item) => {
-            item.style.opacity = "0";
-            item.style.transform = "translateX(-50px)";
-        });
-    });
-});
-
-
-// document.addEventListener("DOMContentLoaded", function () {
-//     const hrElement = document.querySelector(".animated-hr");
-    
-//     setTimeout(() => {
-//         hrElement.classList.add("active");
-//     }, 200); 
-// });
 
 document.addEventListener("DOMContentLoaded",function(){
     const accordions = document.querySelectorAll(".accordian-header");
